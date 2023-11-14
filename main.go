ed56736a1effb4c03dc5c9c8e082c1ddcaefc825
Manuel Rivera <menu.rivera@gmail.com>
2023-11-14 15:06:00 -0600

    [ADD] -> POST /todo request added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -18,13 +19,33 @@ var todos = []todo{
 }
 
 func getTodos(ctx *gin.Context) {
+	fmt.Println("GET todos...")
+	fmt.Println(todos)
 	ctx.IndentedJSON(http.StatusOK, todos)	
 }
 
+func addTodo(ctx *gin.Context) {
+	fmt.Println("POST todo...")
+	var newTodo todo 
+
+	if err := ctx.BindJSON(&newTodo); err != nil {
+		fmt.Println(err)
+		ctx.IndentedJSON(http.StatusServiceUnavailable, err.Error())
+		return
+	}
+	todos = append(todos, newTodo)
+
+	ctx.IndentedJSON(http.StatusCreated, todos)
+}
+
 func main() {
-	router := gin.Default()	
+	// var router = gin.Default() <- equivalent	
+	router := gin.Default() 
 
+	// routes
 	router.GET("/todos", getTodos)
+	router.POST("/todo", addTodo)
 
+	// server init
 	router.Run("localhost:9090")
 }
