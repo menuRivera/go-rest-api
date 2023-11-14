7cac7bbba68d8adf7a8dd1b52e09602cf0a1776f
Manuel Rivera <menu.rivera@gmail.com>
2023-11-14 15:19:54 -0600

    [ADD] -> GET /todos/:id handler added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"errors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,6 +23,30 @@ func getTodos(ctx *gin.Context) {
 	fmt.Println("GET todos...")
 	fmt.Println(todos)
 	ctx.IndentedJSON(http.StatusOK, todos)	
+}
+
+func getTodoById(id string) (*todo, error) {
+	for _, t := range todos {
+		if t.ID == id {
+			return &t, nil
+		}
+	}
+
+	return nil, errors.New("Todo not found")
+}
+
+func getTodo(ctx *gin.Context) {
+	id := ctx.Param("id")	
+
+	fmt.Println(fmt.Sprintf("GET /todos/%s", id))
+
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+	}
+
+	ctx.IndentedJSON(http.StatusOK, todo)
 }
 
 func addTodo(ctx *gin.Context) {
@@ -44,7 +69,8 @@ func main() {
 
 	// routes
 	router.GET("/todos", getTodos)
-	router.POST("/todo", addTodo)
+	router.GET("/todos/:id", getTodo)
+	router.POST("/todos", addTodo)
 
 	// server init
 	router.Run("localhost:9090")
